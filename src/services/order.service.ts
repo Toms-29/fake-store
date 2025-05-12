25cd9c2e15144659b3e407a0e514a6013d8acfb6
Tomas <gamezmatiastomas@gmail.com>
2025-05-12 13:11:44 -0300

    Correccion en el service de oreder:
    Creacion de la funcion confirmPurchase, que se encargara
    de restar la cantidad solicitada en la compra a la
    cantidad total del producto. Tambien se encargara
    de cambiar el status del producto en caso de ser insuficiente.
    
    La funcion verifySattus fue eliminada debido a que
    quedo en desuso.

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,18 +1,35 @@
 import Product from "../models/Product.model.js"
+import Cart from "../models/Cart.model.js"
 import { HttpError } from "../errors/HttpError.js"
+import { CartType, ProductType } from "../types/cart.types.js"
 import { ProductStatus } from "../types/product.types.js"
 
-export const verifySatus = async (id: string, amount: number) => {
-    if (amount === 0) {
-        try {
-            const statusUpdated = await Product.findOneAndUpdate(
-                { userId: id },
-                { $set: { status: ProductStatus.OUT_OF_STOCK } },
-                { new: true }
-            )
-            return statusUpdated
-        } catch (error) {
-            throw new HttpError("Internal server error", 500)
+
+export const confirmPurchase = async (id: string) => {
+    const cartFound = await Cart.findById(id).populate('products.productId').lean() as CartType;
+    if (!cartFound) { throw new HttpError("Cart not found", 404); }
+
+    const updatedProducts = await Promise.all(cartFound.products.map(async (cartItem) => {
+        const product = cartItem.productId as ProductType;
+        const id = product._id.toString();
+        const quantity = cartItem.quantity;
+        const newAmount = product.amount - quantity;
+
+        if (newAmount < 0) { throw new HttpError("Insufficient amount", 409); }
+
+        const updateFields: Partial<ProductType> = {
+            amount: newAmount,
+        };
+
+        if (newAmount === 0) {
+            updateFields.status = ProductStatus.OUT_OF_STOCK;
         }
-    } else { return }
-}
+
+        return await Product.findByIdAndUpdate(
+            id,
+            { $set: updateFields },
+            { new: true }
+        );
+    }));
+    return updatedProducts;
+};
