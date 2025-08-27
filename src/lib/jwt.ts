d8a7179ee601bb030e467c5d613081d563601448
Tomas <gamezmatiastomas@gmail.com>
2025-08-27 09:11:40 -0300

    Corrigiendo error de importacion de la dependencia jwt en /lib/jwt

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,7 +1,8 @@
-import { sign } from 'jsonwebtoken'
+import jsonwebtoken from 'jsonwebtoken'
 import { Types } from "mongoose"
 import { ENV } from '../config/env.js'
 
+const { sign } = jsonwebtoken
 
 export const createAccessToken = (payload: { id: Types.ObjectId, role: string }): string => {
     return sign(payload, ENV.SECRET_TOKEN_KEY, { expiresIn: '15m' })
